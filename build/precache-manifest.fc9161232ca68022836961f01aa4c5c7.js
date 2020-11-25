15bc9958aa4fe26b1bbd37c85ccbf1c2f14e10d4
accimeesterlin <accimeesterlin@yahoo.com>
2020-11-25 00:27:33 -0500

    adding trustpilot and fix warning

---
+++ build/precache-manifest.fc9161232ca68022836961f01aa4c5c7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e9a4470cb0b090d962a7eb3a32c809",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05fe1ddb47ca0a99a7fd",
+    "url": "/static/css/main.28667364.chunk.css"
+  },
+  {
+    "revision": "a5306840473bba4cfdb9",
+    "url": "/static/js/2.816ab0cb.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.816ab0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fe1ddb47ca0a99a7fd",
+    "url": "/static/js/main.7244ede2.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "1a3ad51cf6ecb562a2d2fb3047a2b016",
+    "url": "/static/media/amazon full listing and ads campaign ecombuilder.1a3ad51c.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "c1b81f1954848b62d4e230448fdbd2e4",
+    "url": "/static/media/dropshipping facebook video ads ecombuilder-01.c1b81f19.png"
+  },
+  {
+    "revision": "f0fa9379cb180518f7125d438180919f",
+    "url": "/static/media/e-combuilder shopify website design.f0fa9379.png"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "ffbdbd9dbf1cbfb8dd8bb28d67def20a",
+    "url": "/static/media/logo.ffbdbd9d.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
--- src/components/sections/TrustPilotReviews.js
+++ src/components/sections/TrustPilotReviews.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import classNames from 'classnames';
-import { SectionTilesProps } from '../../utils/SectionProps';
-import SectionHeader from './partials/SectionHeader';
+import React from "react";
+import { SectionTilesProps } from "../../utils/SectionProps";
 
 const propTypes = {
-  ...SectionTilesProps.types
-}
+  ...SectionTilesProps.types,
+};
 
 const defaultProps = {
-  ...SectionTilesProps.defaults
-}
+  ...SectionTilesProps.defaults,
+};
 
 const Testimonial = ({
   className,
@@ -22,20 +20,30 @@ const Testimonial = ({
   pushLeft,
   ...props
 }) => {
-
-
   return (
-    <section
-      {...props}
-    >
-      <div className="container">
-        <div class="trustpilot-widget" data-locale="en-US" data-template-id="5613c9cde69ddc09340c6beb" data-businessunit-id="5f74ddfff2c2360001518f7a" data-style-height="100%" data-style-width="100%" data-theme="light">
-        <a style={{ color: "#FFFFFF" }} href="https://www.trustpilot.com/review/ecombrandbuilder.com" target="_blank" rel="noopener">Trustpilot</a>
-        </div>
+    <section {...props}>
+      {/* TrustBox widget - Micro Review Count */}
+      <div
+        className="trustpilot-widget container section-inner pt-0"
+        data-locale="en-US"
+        data-template-id="5419b6a8b0d04a076446a9ad"
+        data-businessunit-id="5f74ddfff2c2360001518f7a"
+        data-style-height="24px"
+        data-style-width="100%"
+        data-theme="light"
+      >
+        <a
+          href="https://www.trustpilot.com/review/ecombrandbuilder.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Trustpilot
+        </a>
       </div>
+      {/* End TrustBox widget */}
     </section>
   );
-}
+};
 
 Testimonial.propTypes = propTypes;
 Testimonial.defaultProps = defaultProps;
